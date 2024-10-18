4c05afec948890087295fa2133eaacbe33ce38b1
Guilh3rme <guilhermereisefr@gmail.com>
2024-10-18 10:09:53 -0300

    fix: desabling-eslint-error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
 import EmblaCarousel from "@/app/components/Carousel/EmblaCarousel";
 import { EmblaOptionsType } from "embla-carousel";
